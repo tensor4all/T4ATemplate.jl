f5eccd6453607162c4febd6271d81379d5fcc5f3
Documenter.jl <documenter@juliadocs.github.io>
2024-11-05 14:29:02 +0000

    build based on 94e8888

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = T4ATemplate","category":"page"},{"location":"#T4ATemplate","page":"Home","title":"T4ATemplate","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"This is the documentation for T4ATemplate.","category":"page"}]
+}
